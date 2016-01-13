9ee45fa208638d6e6b0358b883be4dbae5d921ae
crazy50bmg <crazy50bmg@gmail.com>
2016-01-12 20:31:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -515,9 +515,12 @@ function Sentry_onClick(theID) {
       // submit the form
       
       var frame = window.frameElement;  // Get the <iframe> element of the window
-
+window.alert("Here we go...");
 if (frame) {   // If the window is in an <iframe>, change its source
     window.alert(frame.name);
+} else {
+  window.alert("Nope");
+  
 }
   
       
